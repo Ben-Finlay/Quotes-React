5263e0e9966399b8c3315a09def51f8ad0706ffe
Ben Finlay <benjamin.finlay@gmail.com>
2022-10-27 17:48:39 -0400

    attempt to connect db again

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,10 +1,10 @@
 let dbParams = {};
   dbParams = {
-    host: 'dpg-cddfakhgp3jnebdgm94g-a',
-    port: 5432,
-    user: 'react',
-    password: 'YhPlhNWKAm0CRZoBYiKG9gT9wMrdw9EN',
-    database: 'allquotes'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   };
 
 
